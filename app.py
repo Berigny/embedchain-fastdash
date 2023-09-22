6785b546d21092c96cfd13285530ea7d48e35bf4
Berigny <134203992+Berigny@users.noreply.github.com>
2023-09-22 05:24:48 +0000

    Change

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-import os
-from fast_dash import FastDash, Fastify, dcc, dmc
 
+import streamlit as st
+import os
+from string import Template
 from embedchain import App
 from embedchain.config import QueryConfig
-from string import Template
 
 # Define app configurations
 PROMPT = Template(
-    """Use the given context to answer the question at the end.
+    '''Use the given context to answer the question at the end.
 If you don't know the answer, say so, but don't try to make one up.
 At the end of the answer, also give the sources as a bulleted list.
 Display the answer as markdown text.
@@ -16,59 +16,15 @@ Context: $context
 
 Query: $query
 
-Answer:"""
+Answer:'''
 )
 query_config = QueryConfig(
     template=PROMPT, number_documents=5, max_tokens=2000, model="gpt-4"
 )
 
-# Define components
-openai_api_key_component = dmc.PasswordInput(
-    placeholder="API Key",
-    description="Get yours at https://platform.openai.com/account/api-keys",
-    required=True,
-)
-
-web_page_urls_component = dmc.MultiSelect(
-    description="Include all the reference web URLs",
-    placeholder="Enter URLs separated by commas",
-    searchable=True,
-    creatable=True,
-)
-
-text_component = dmc.Textarea(
-    placeholder="Write your query here",
-    autosize=True,
-    minRows=4,
-    description="Any additional information that could be useful",
-)
-
-query_component = dmc.Textarea(
-    placeholder="Write your query here",
-    autosize=True,
-    minRows=4,
-    required=True,
-    description="Write your query here",
-)
-
-answer_component = dcc.Markdown(
-    style={"text-align": "left", "padding": "1%"}, link_target="_blank"
-)
-
-
 def explore_your_knowledge_base(
-    openai_api_key: openai_api_key_component,
-    web_page_urls: web_page_urls_component,
-    youtube_urls: web_page_urls_component,
-    pdf_urls: web_page_urls_component,
-    text: text_component,
-    query: text_component,
-) -> answer_component:
-    """
-    Input your sources and let GPT4 find answers. Built with Fast Dash.
-    This app uses embedchain.ai, which abstracts the entire process of loading and chunking datasets, creating embeddings, and storing them in a vector database.
-    Embedchain itself uses Langchain and OpenAI's ChatGPT API.
-    """
+    openai_api_key, web_page_urls, youtube_urls, pdf_urls, text, query
+):
     answer_suffix = ""
 
     if not openai_api_key:
@@ -98,20 +54,25 @@ def explore_your_knowledge_base(
         answer_suffix = "I couldn't analyze some sources. If you think this is an error, please try again later or make a suggestion [here](https://github.com/dkedar7/embedchain-fastdash/issues)."
 
     answer = app.query(query, query_config)
-    answer = f"""{answer}
+    answer = f'''{answer}
 
     {answer_suffix}
-    """
+    '''
 
     return answer
 
+# Streamlit layout and interactivity
+st.title("Explore Your Knowledge Base")
 
-# Build app (this is all it takes!). Fast Dash understands what it needs to do.
-app = FastDash(
-    explore_your_knowledge_base,
-    github_url="https://github.com/dkedar7/embedchain-fastdash",
-)
-server = app.server
+openai_api_key = st.text_input("API Key", help="Get yours at https://platform.openai.com/account/api-keys")
+web_page_urls = st.text_area("Include all the reference web URLs", help="Enter URLs separated by commas")
+youtube_urls = st.text_area("Include all the reference YouTube URLs", help="Enter URLs separated by commas")
+pdf_urls = st.text_area("Include all the reference PDF URLs", help="Enter URLs separated by commas")
+text = st.text_area("Any additional information that could be useful")
+query = st.text_area("Write your query here", help="Write your query here")
 
-if __name__ == "__main__":
-    app.run()
+if st.button("Submit"):
+    answer = explore_your_knowledge_base(
+        openai_api_key, web_page_urls.split(","), youtube_urls.split(","), pdf_urls.split(","), text, query
+    )
+    st.markdown(answer)
